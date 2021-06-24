badf00f109564077108dd5f70b24fb0206818e22
Sam Hannan <samhannan47@gmail.com>
2021-06-24 16:25:11 -0400

    Big refactor of boilerplate to use React Hooks & RTK

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,47 +1,52 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { handleLogout } from "../store";
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>FS-App-Template</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+const Navbar = () => {
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
+  const dispatch = useDispatch();
+  const name = useSelector((state) => state.auth.username);
+  const handleClick = () => {
+    dispatch(handleLogout());
+  };
+  return (
+    <div id="navbar">
+      <h1 className="title">Sam's Stackathon</h1>
+      <nav>
+        {isLoggedIn ? (
+          <span id="logged-in">
+            {/* The navbar will show these links after you log in */}
+            <span>Logged in as {name}</span>
+            <button className="button is-danger" type="button">
+              <Link to="/" onClick={handleClick}>
+                Logout
+              </Link>
+            </button>
+          </span>
+        ) : (
+          <span id="login">
+            {/* The navbar will show these links before you log in */}
+            <button className="button is-danger" type="button">
+              <Link to="/login">Login</Link>
+            </button>
+            <button className="button is-danger" type="button">
+              <Link to="/signup">Sign Up</Link>
+            </button>
+          </span>
+        )}
+        <span id="products">
+          <button className="button is-danger" type="button">
+            <Link to="/train">Get Going Here!</Link>
+          </button>
+          <button className="button is-danger" type="button">
+            <Link to="/account">My Account</Link>
+          </button>
+        </span>
+      </nav>
+      <hr id="nav-break" />
+    </div>
+  );
+};
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.auth.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Navbar)
+export default Navbar;
---
+++ client/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1 className="subtitle">Welcome to the Sam's Stackathon</h1>
+      <p>FILLERRRRRR</p>
+      <h2>Hi!</h2>
+    </div>
+  );
+};
+
+export default Welcome;
