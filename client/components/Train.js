0329493ce5457422182ec282a1b41d7458fa25a3
Sam Hannan <samhannan47@gmail.com>
2021-06-25 10:26:49 -0400

    added toast warnings to button if not training yet

--- client/components/Train.js
+++ client/components/Train.js
@@ -2,14 +2,50 @@ import { render } from "enzyme";
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import app from "../../public";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+let isTraining = 0;
 const Train = () => {
+  const notify = () =>
+    toast.warn("you need to start training!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
+  const sum = () => {
+    isTraining = isTraining + 1;
+  };
+
+  const handleClick = () => {
+    if (isTraining === 0) {
+      notify();
+    }
+  };
   return (
     <div>
-      <button onClick={() => app()}>Train</button>
-      <button id="class-a">Add A</button>
-      <button id="class-b">Add B</button>
-      <button id="class-c">Add C</button>
+      <button onClick={() => { app(); sum()}}>Train</button>
+      <button id="class-a" onClick={() => handleClick()}>
+        Add A
+      </button>
+      <button id="class-b" onClick={() => handleClick()}>Add B</button>
+      <button id="class-c" onClick={() => handleClick()}>Add C</button>
+      <ToastContainer
+        position="top-right"
+        autoClose={500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 };
